f8aadfb6b921aba017693cab55ea8ec0006d5760
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-05-10 18:00:17 +0200

    Show/hide blog detail button added

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+	const [showDetails, setShowDetails] = useState(false)
+
+	return (
+		<div>
+			<p>
+				{blog.title} {blog.author}
+				<span>
+					<button
+						onClick={() => {
+							setShowDetails(!showDetails)
+						}}
+					>
+						View
+					</button>
+				</span>
+			</p>
+
+			{showDetails && (
+				<div>
+					<p>{blog.url}</p>
+					<p>{blog.likes}</p>
+					<p>Creator</p>
+				</div>
+			)}
+		</div>
+	)
+}
 
 export default Blog
